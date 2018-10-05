8bbb81e6addc62ae2f1e685d45ea19b3bec0b3f0
Carl Garner <carl.garner@piksel.com>
2018-10-05 08:20:23 +0100

    Disable refresh option for now

--- manifests/key.pp
+++ manifests/key.pp
@@ -50,7 +50,7 @@ define apt::key (
       if !defined(Anchor["apt_key ${id} present"]) {
         apt_key { $title:
           ensure  => present,
-          refresh => $ensure == 'refreshed',
+          #refresh => $ensure == 'refreshed',
           id      => $id,
           source  => $source,
           content => $content,
